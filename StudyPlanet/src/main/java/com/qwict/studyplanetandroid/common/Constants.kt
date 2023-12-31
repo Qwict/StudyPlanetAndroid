9b577fee8904361dfb143b7e0bf4f26a95e18cfd
jorisvanduyse <jorisduyse@gmail.com>
2023-12-31 20:58:21 +0100

    v0.9.12 -> Fixed Constants

--- StudyPlanet/src/main/java/com/qwict/studyplanetandroid/common/Constants.kt
+++ StudyPlanet/src/main/java/com/qwict/studyplanetandroid/common/Constants.kt
@@ -6,17 +6,21 @@ import com.qwict.studyplanetandroid.domain.model.User
 
 object Constants {
     const val BASE_URL = "https://sp.qwict.com/api/"
-//        "http://192.168.0.224:9012/api/"
+//        "http://192.168.1.36:9012/api/"
 //        "http://10.0.2.2:9012/api/"
-//        "http://192.168.1.33:9012/api/"
+//        "http://192.168.0.224:9012/api/"
 
-    val DEFAULT_PLANET_IMAGE_ID = R.drawable.study_planet_icon
+    val DEFAULT_PLANET_IMAGE_ID = R.drawable.galaxy
+    val DEFAULT_PLANET_SMALL_IMAGE_ID = R.drawable.galaxy_small
     val EMPTY_USER = User(
         discoveredPlanets = emptyList(),
         email = "",
         experience = 0,
-        id = 0,
         name = "",
+        currentLevel = 0,
+        experienceForCurrentLevel = 0,
+        experienceForNextLevel = 0,
+        experienceProgress = 0.0f,
     )
     val EMPTY_PLANET = Planet(
         id = 0,
